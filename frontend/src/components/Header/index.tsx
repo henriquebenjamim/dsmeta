844f2d30d453faa9664af35e2c8a613999c443dd
henriquebenjamim <henriiqqq@gmail.com>
2022-11-14 18:56:46 -0300

    Datepicker

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,11 +9,11 @@ function Header() {
             <h1>DSMeta</h1>
             <p>
               Desenvolvido por
-              <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+              <a href="https://www.instagram.com/henrique.benjamim">@henrique.benjamim</a>
             </p>
         </div>
     </header>
   )
 }
 
-export default Header
+export default Header;
