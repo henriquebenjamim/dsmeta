c190f1e06e97847399aa84dd60a0461e09484723
henriquebenjamim <henriiqqq@gmail.com>
2022-11-19 16:59:09 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import { ToastContainer } from 'react-toastify';
+import { Toast } from 'react-toastify/dist/components';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Header from "./components/Header";
 import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
